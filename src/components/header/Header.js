d79ad1c0605ded72f99779e6c44a22ed437fd5f8
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-05-22 04:44:20 +0300

    changed font

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div className="headerComponent">Header</div>;
+  return (
+    <div className="headerComponent">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ex unde
+      repellat quae voluptate. Repellendus deleniti sit provident exercitationem
+      distinctio, magni dolorum, molestiae cum necessitatibus incidunt tempore
+      eligendi, laudantium voluptatum?
+    </div>
+  );
 };
 
 export default Header;
