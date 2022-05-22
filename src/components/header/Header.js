b0f1d6931bd490f1a2d5da9dece2a32179bf5a02
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-05-22 05:05:47 +0300

    fallback

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./header.css";
+import image from "../../assets/images/pp.jpg";
 
 const Header = () => {
   return (
-    <div className="headerComponent">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ex unde
-      repellat quae voluptate. Repellendus deleniti sit provident exercitationem
-      distinctio, magni dolorum, molestiae cum necessitatibus incidunt tempore
-      eligendi, laudantium voluptatum?
+    <div className="headerComponent d-flex flex-column flex-md-row justify-content-center">
+      <div className="textGroup">
+        <div className="bigTitle">
+          {" "}
+          Hi, <span className="title"> my name is Murat</span>{" "}
+        </div>
+        <div className="para">I'm a web developer</div>
+        <div className="para">
+          I build things for web 3 as well as the good ol' fashioned web 2
+        </div>
+      </div>
+      <div className="imageContainer">
+        <img src={image} alt="" className="profileImage" />
+      </div>
     </div>
   );
 };
