eedd90399fa4e262368882984525cb47c63ec692
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-09-06 02:33:00 +0300

    fallback

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,26 +1,68 @@
 import React from "react";
 import "./about.css";
 const About = () => {
+  const techStack = [
+    "HTML",
+    "CSS",
+    "JavaScript (ES6)",
+    "React",
+    "VueJs",
+    "Redux",
+    "Node",
+    "Express",
+    "MongoDB",
+    "Mongoose",
+    "Git",
+    "GitHub",
+    "Bootstrap",
+    "Solidity",
+    "Hardhat",
+    "EthersJs",
+    "Jest",
+    "Mocha",
+    "Chai",
+    "React Testing Library",
+    "Heroku",
+    "Netlify",
+    "Postman",
+    "Jira",
+  ];
+  const education = [
+    {
+      name: "Semiotics MA",
+      university: "University of Tartu/Estonia",
+      year: "2020",
+    },
+    {
+      name: "Semiotics MA w/ Erasmus+ Scholarship",
+      university: "L'Université de Bordeaux-Montaigne/France",
+      year: "2019",
+    },
+    {
+      name: "Comparative Litarature BA w/ Erasmus+ Scholarship",
+      university: "Eötvös Lorand University/Budapest",
+      year: "2016",
+    },
+    {
+      name: "Comparative Litarature BA",
+      university: "Eskişehir Osmangazi University/Eskişehir",
+      year: "2020",
+    },
+  ];
   return (
     <div id="aboutComponent   ">
       <div className="aboutWrapper d-flex flex-column-reverse flex-md-column">
         <div className="above d-flex flex-column flex-md-row justify-content-md-around">
-          <div className="techStack d-flex justify-content-center">
+          <div className="techStack d-flex flex-column justify-content-center">
+            <div className="aboutTitle d-flex justify-content-center">
+              <div>My Tech Stack</div>
+            </div>
             <ul className="stacks d-flex justify-content-center flex-wrap ">
-              <li className="stack">HTML</li>
-              <li className="stack">CSS</li>
-              <li className="stack">Javascript</li>
-              <li className="stack">ReactJs</li>
-              <li className="stack">VueJs</li>
-              <li className="stack">Solidity</li>
-              <li className="stack">Hardhat</li>
-              <li className="stack">EthersJs</li>
-              <li className="stack">Jest</li>
-              <li className="stack">Mocha</li>
-              <li className="stack">React Testing Library</li>
-              <li className="stack">Git</li>
-              <li className="stack">MongoDB</li>
-              <li className="stack">Bootstrap</li>
+              {techStack.map((stack, index) => (
+                <li key={index} className="stack">
+                  {stack}
+                </li>
+              ))}
             </ul>
           </div>
           <div className="education">education</div>
