c61e07e4be8b47e6845fd135a040f575fb1ff9e7
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-09-06 01:59:00 +0300

    doing hardcore css stuff, fallback

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,7 +1,31 @@
 import React from "react";
-
+import "./about.css";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div id="aboutComponent d-flex flex-column ">
+      <div className="above d-flex flex-column flex-md-row justify-content-md-around">
+        <div className="techStack d-flex justify-content-center">
+          <ul className="stacks d-flex justify-content-center flex-wrap ">
+            <li className="stack">item</li>
+            <li className="stack">item</li>
+            <li className="stack">item</li>
+            <li className="stack">item</li>
+            <li className="stack">item</li>
+            <li className="stack">item</li>
+            <li className="stack">item</li>
+            <li className="stack">item</li>
+            <li className="stack">item</li>
+            <li className="stack">item</li>
+            <li className="stack">item</li>
+          </ul>
+        </div>
+        <div className="education">education</div>
+      </div>
+      <div className="below d-flex justify-content-center">
+        <div className="work">work</div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
