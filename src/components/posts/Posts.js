df2fde279dcb31720765b5297e6703dcaef8c3d9
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-09-07 03:09:26 +0300

    fallback

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -44,40 +44,51 @@ const Posts = () => {
   }, []);
   return (
     <div>
-      <div className="postsTitle">Blog Posts</div>{" "}
-      <div className="wrapper d-flex justify-content-center flex-wrap">
-        {data.map((item) => {
-          return (
-            <div
-              //this card is coming from bootstrap
-              //it might create problems in deployment
-              className="card d-flex flex-column justify-content-between align-items-end"
-              key={item.id}
-              style={{ background: item.cover_image }}
-            >
-              <div className="postTitle">{item.title}</div>
-              <a href={item.url} target="_blank" rel="noreferrer">
-                <Button
-                  backgroundColor="white"
-                  border="1px solid rgba(0,124,137,.3)"
-                  borderHover="1px solid  #004e56"
-                  color="#004e56"
-                  fontSize="1rem"
-                  height="3.2rem"
-                  width="auto"
-                >
-                  <div style={{ fontSize: "0.9rem" }}>Read More</div>{" "}
-                </Button>
-              </a>{" "}
-            </div>
-          );
-        })}
+      <div className="postsWrapper d-flex flex-column align-items-center">
+        <div className="postsBigTitle">Blog Posts</div>{" "}
+        <div className="wrapper d-flex align-items-center justify-content-center flex-wrap">
+          {data.map((item) => {
+            return (
+              <div
+                //this card is coming from bootstrap
+                //it might create problems in deployment
+                className="card d-flex flex-column justify-content-between align-items-end"
+                key={item.id}
+                style={{ background: item.cover_image }}
+              >
+                <div className="postTitle">{item.title}</div>
+                <a href={item.url} target="_blank" rel="noreferrer">
+                  <Button
+                    backgroundColor="white"
+                    border="1px solid rgba(0,124,137,.3)"
+                    borderHover="1px solid  #004e56"
+                    color="#004e56"
+                    fontSize="1rem"
+                    height="3.2rem"
+                    width="auto"
+                  >
+                    <div style={{ fontSize: "0.9rem" }}>Read More</div>{" "}
+                  </Button>
+                </a>{" "}
+              </div>
+            );
+          })}
+        </div>
+        <Button
+          backgroundColor="white"
+          border="1px solid rgba(0,124,137,.3)"
+          borderHover="1px solid  #004e56"
+          color="#004e56"
+          fontSize="1.2rem"
+          height="3.4rem"
+          width="auto"
+          className="showBtn"
+          onClick={showHide}
+        >
+          {" "}
+          {showMore ? "Show more posts" : "Show less posts"}{" "}
+        </Button>
       </div>
-      <div className="fillerDots">...</div>
-      <button onClick={showHide}>
-        {" "}
-        {showMore ? "Show more posts" : "Show less posts"}{" "}
-      </button>
     </div>
   );
 };
